f97ccbb96896f825c3ab879c6bfe599fedc244ab
Ben Sidelinger <ben_sidelinger@discovery.com>
2016-06-22 16:26:33 -0700

    finished rework to always trim down at the end

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import ResizeCore from './ResizeCore';
 
 const sizerWrapperStyles = {
   position: 'absolute',
-  left: '0px',
+  left: '-100000px',
   top: '200px',
   width: '100%',
 };
@@ -27,54 +27,63 @@ export default class extends ResizeCore {
     children: PropTypes.string.isRequired,
   }
 
-  checkHeight(adjustDown) {
-    const targetHeight = ReactDOM.findDOMNode(this.refs.sizer).offsetHeight;
+  _checkHeight(adjustDown) {
     const contentHeight = ReactDOM.findDOMNode(this.refs.testChildren).offsetHeight;
-    return (adjustDown) ? (contentHeight <= targetHeight) : (contentHeight > targetHeight);
+    return (adjustDown) ? (contentHeight <= this._targetHeight) : (contentHeight > this._targetHeight);
   }
 
   // this function will add everything then remove one at a time until the desired height is obtained
-  adjustDown() {
-    console.log('adjust down');
+  _adjustDown() {
     if (this.state.testChildren === '') {
       this.setState({ testChildren: this.props.children });
-    } else if (this.checkHeight(true)) {
-      console.log('likes the height');
-      this.setChildren(true);
+      setTimeout(() => { this._adjustDown(); }, 0);
+    } else if (this._checkHeight(true)) {
+      this._setChildren();
     } else {
-      console.log('should be trimming one');
       this.setState({ testChildren: this.state.testChildren.slice(0, -1) });
-      setTimeout(() => { this.adjustDown(); }, 0);
+      setTimeout(() => { this._adjustDown(); }, 0);
     }
   }
 
-  adjustUp() {
-    if (this.checkHeight(false)) {
-      this.setChildren(false);
+  _adjustUp() {
+    // have we used all our characters?
+    if (this._checkHeight(false)) {
+      setTimeout(() => { this._adjustDown(); }, 0);
     } else if (this.state.testChildren.length !== this.props.children.length) {
       this.setState({ testChildren: this.props.children.substring(0, this.state.testChildren.length + 1) });
-      setTimeout(() => { this.adjustUp(); }, 0);
+      setTimeout(() => { this._adjustUp(); }, 0);
+    } else {
+      this._setChildren();
     }
   }
 
-  setChildren(adjustDown) {
-    const toRemove = (adjustDown) ? -2 : -3;
-    const children = this.state.testChildren.slice(0, toRemove).split(' ').slice(0, -1);
-    this.setState({ children: `${children.join(' ')}...` });
+  _setChildren() {
+    let children = this.props.children;
+
+    // are we actually trimming?
+    if (this.state.testChildren.length < this.props.children.length) {
+      children = this.state.testChildren.slice(0, -3).split(' ').slice(0, -1);
+      children = `${children.join(' ')}...`;
+    }
+
+    this.setState({ children }); // , fixHeight: null });
   }
 
-  // this is the meat of the component,
-  // it adds the trimmed content to state and fills the sizer on resize events
+  // adds the trimmed content to state and fills the sizer on resize events
   handleResize() {
     const availableWidth = ReactDOM.findDOMNode(this.refs.content).offsetWidth;
+    this._targetHeight = ReactDOM.findDOMNode(this.refs.sizer).offsetHeight;
+
+    // set the max height right away, so that the resize throttle doesn't allow line break jumps
+    this.setState({ fixHeight: this._targetHeight });
 
     // was there a width change?
     if (availableWidth !== this.state.lastCalculatedWidth) {
       // first render?
       if (this.state.children === '' || availableWidth < this.state.lastCalculatedWidth) {
-        this.adjustDown();
+        this._adjustDown();
       } else {
-        this.adjustUp();
+        this._adjustUp();
       }
     }
   }
@@ -83,9 +92,11 @@ export default class extends ResizeCore {
     const { fixHeight, children, testChildren } = this.state;
 
     const wrapperStyles = { position: 'relative' };
+
+    // the idea here is to fix the height while were adding/removing from the test container
     if (fixHeight) {
-      wrapperStyles.height = `${fixHeight}px`;
-      // wrapperStyles.overflow = 'hidden';
+      wrapperStyles.maxHeight = `${fixHeight}px`;
+      wrapperStyles.overflow = 'hidden';
     }
 
     const vertSpacers = [];
